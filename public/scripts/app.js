fbd543823ad4a242b4ab722a3fdf30bc9bdf0aab
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-09-23 07:36:04 +0100

    Quick fix kelvin to celsius for forecast. Tidies up UI slightly

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
       $("#temp").css("color", "orange");
     } else if(thermostat.temp >= 25) {
       $("#temp").css("color", "red");
-    };
+    }
   };
 
   function firstTemp() {
@@ -17,12 +17,12 @@ $( document ).ready(function() {
         refreshTemp();
         // PUT THE REFRESHTEMP CALL INSIDE THE GET FUNCTION SO THAT IT WONT EXECUTE UNTIL THE GET IS FINISHED.
       });
-  };
+  }
 
   function refreshTemp() {
     colour();
     $("#temp").html(thermostat.temp);
-  };
+  }
 
   var city = "London";
 
@@ -30,21 +30,31 @@ $( document ).ready(function() {
 
     $.getJSON('http://api.openweathermap.org/data/2.5/find?q=' + city + '&units=metric',
       function (data) {
-        $('#currentCity').html(city + " current temp");
-        $('#cityWeather').html(data.list[0].main.temp);
+        $('#currentCity').html("Current temperature in " + city);
+        $('#cityWeather').html(data.list[0].main.temp + " C");
     });
 
     // the function(data) {} part is the 'callback' - the function that runs when ajax call is finished. Part of the JavaScript asynchronous thing.
+  };
+
+  var loadForecast = function() {
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&units=metric,us&mode=json',
+      function (data) {
+        $('#forecastCity').html("Today\'s forecast for " + city)
+        $('#forecast').html((parseInt(data.list[0].temp.day) - 273.15).toFixed(2) + " C");
+    });
   };
 
   $('#cityId').submit(function(e) {
     e.preventDefault();
     city = $('#cityName').val();
     cityWeather();
+    loadForecast();
   });
 
   firstTemp();
   cityWeather();
+  loadForecast();
 
   $("#up").click(function() {
     thermostat.up();
--- public/scripts/thermostat.js
+++ public/scripts/thermostat.js
@@ -1,18 +1,18 @@
 function Thermostat() {
   this.temp = 20;
   this.powerSave = true;
-};
+}
 
 Thermostat.prototype.up = function() {
   if(this.powerSave) {
     if(this.temp + 1 > 25) {
-      this.temp = 25
+      this.temp = 25;
     } else {
       this.temp ++;
     }
   } else {
     if(this.temp + 1 > 32) {
-      this.temp = 32
+      this.temp = 32;
     } else {
       this.temp ++;
     }
@@ -30,16 +30,16 @@ Thermostat.prototype.down = function() {
 
 Thermostat.prototype.reset = function() {
   this.temp = 20;
-}
+};
 
 Thermostat.prototype.toggle = function() {
-  if (this.powerSave == true) {
+  if (this.powerSave === true) {
     this.powerSave = false;
   }
-  else if (this.powerSave == false) {
+  else if (this.powerSave === false) {
     this.powerSave = true;
     if (this.temp > 25) {
       this.temp = 25;
-    };
-  };
+    }
+  }
 };
